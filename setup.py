876638335a4d2b5f0ad5d0fcb5458eda06d8842a
Lion Krischer <lion.krischer@gmail.com>
2018-12-12 10:19:40 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_analysis",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example data analysis package",
+    install_requires=("numpy", "requests"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/opengeophysics/2018-agu-oss-example-repo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
