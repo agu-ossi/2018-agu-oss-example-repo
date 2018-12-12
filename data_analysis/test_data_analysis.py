eacb151a663055915bb8012956bbd3070237391e
Lion Krischer <lion.krischer@gmail.com>
2018-12-12 10:28:05 -0500

    Use proper numpy testing framework.

--- data_analysis/test_data_analysis.py
+++ data_analysis/test_data_analysis.py
@@ -4,4 +4,6 @@ from .functions import moving_average
 
 
 def test_moving_average():
-    assert np.all(moving_average(np.ones(10) * 1.000000001, 4)[2:-2] == 1)
+    np.testing.assert_allclose(
+        moving_average(np.ones(10) * 1.000000001, 4)[2:-2],
+        1)
