0309480e2ad8cf26d1bb67b3b5c70a99a2102c16
Lion Krischer <lion.krischer@gmail.com>
2018-12-12 10:25:23 -0500

    failing test.

--- data_analysis/test_data_analysis.py
+++ data_analysis/test_data_analysis.py
@@ -4,4 +4,4 @@ from .functions import moving_average
 
 
 def test_moving_average():
-    assert np.all(moving_average(np.ones(10), 4)[2:-2] == 1)
+    assert np.all(moving_average(np.ones(10) * 1.000000001, 4)[2:-2] == 1)
